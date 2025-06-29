816b7f6512a629f5b139f7abfb55ee3e3c109785
Shubham Pandit <shubhampandit1125@gmail.com>
2025-06-29 17:05:22 +0100

    Modified security logic v2.

--- src/main/java/com/shubham/saas/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/shubham/saas/security/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
@@ -30,31 +31,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("Authorization");
 
-        if (header != null && header.startsWith("Bearer ")){
+        if(null == header || !header.startsWith("Bearer")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
             String token = header.substring(7);
-            try {
-                Claims claims = jwtProvider.extractClaims(token);
+            Claims claims = jwtProvider.extractClaims(token);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-                if (jwtProvider.isValidToken(claims)){
-                    String email = claims.getSubject();
-                    String tenant = (String) claims.get("tenant");
+            if (jwtProvider.isValidToken(claims) && null == authentication){
+                String email = claims.getSubject();
+                String tenant = (String) claims.get("tenant");
 
-                    if(null != tenant){
-                        TenantContext.setTenantId(tenant);
-                    }
-
-                    var user = userRepository
-                            .findByEmail(email)
-                            .orElseThrow(() -> new RuntimeException("User not found"));
-
-                    var auth = new UsernamePasswordAuthenticationToken(user.getUsername(), null, user.getAuthorities());
-
-                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(auth);
+                if(null != tenant){
+                    TenantContext.setTenantId(tenant);
                 }
-            } catch (Exception e){
-                SecurityContextHolder.clearContext();
+
+                var user = userRepository.findByEmail(email)
+                        .orElseThrow(() -> new RuntimeException("Invalid Credential"));
+                var auth = new UsernamePasswordAuthenticationToken(user.getUsername(), null, user.getAuthorities());
+
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
+        } catch (Exception e){
+            SecurityContextHolder.clearContext();
         }
         filterChain.doFilter(request, response);
     }
